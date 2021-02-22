1e1a87ecfac8615a8ff6e5d45b7e5402025c50f0
Alec <petrack.2@wright.edu>
2021-02-21 20:05:32 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# PORTFOLIO-APP 
-My portfolio application built with React.
-##### deployed app : https://apetrack13.github.io/Portfolio/
-# Portfolio
---
+++ precache-manifest.9c44959ced85d354dbbd343cd074efce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11c8ba99001661bf9d07",
+    "url": "/Portfolio/static/css/main.2c271d60.chunk.css"
+  },
+  {
+    "revision": "11c8ba99001661bf9d07",
+    "url": "/Portfolio/static/js/main.11c8ba99.chunk.js"
+  },
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c812ddaba4e94678b28583aa37510f36",
+    "url": "/Portfolio/index.html"
+  }
+];
