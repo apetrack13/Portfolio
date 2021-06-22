fa851b8024a5e511935c95bb5344dff3d8b81ebf
Alec Petrack <alecpetrack@Alecs-MacBook-Pro.local>
2021-06-21 22:06:10 -0400

    Updates

--- precache-manifest.050054f2b730c538642cc13c4f9e6334.js
+++ precache-manifest.050054f2b730c538642cc13c4f9e6334.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2756c79df8965bda12ac",
+    "revision": "d362139509bfa6238bb0",
     "url": "/Portfolio/static/css/main.2c271d60.chunk.css"
   },
   {
-    "revision": "2756c79df8965bda12ac",
-    "url": "/Portfolio/static/js/main.2756c79d.chunk.js"
+    "revision": "d362139509bfa6238bb0",
+    "url": "/Portfolio/static/js/main.d3621395.chunk.js"
   },
   {
     "revision": "591eb5beb99e69ed8d3e",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "f4033134d445a9205c4e41f6f68dd4c9",
+    "revision": "49494a8c16840c5138b9df6b952b8c2d",
     "url": "/Portfolio/index.html"
   }
 ];
